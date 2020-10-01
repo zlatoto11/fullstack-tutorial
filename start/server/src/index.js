4029ce0828bbd3dae5916dc1ede888e56ac98225
Zlatomir Kosev <zlatomir_hk@hotmail.com>
2020-10-01 17:16:08 +0100

    GraphQL with React tutorial

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,10 @@
-require('dotenv').config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
+
+require("dotenv").config();
